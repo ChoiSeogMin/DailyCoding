f95cd2cbe9ba03703ecf50891d47d60bf2dddc18
SeogMin <csm0594@gmail.com>
2018-03-09 09:48:05 +0900

    no message

--- DailyCoding/src/Codility/OddOccurrencesInArray/OddOccurrencesInArray.java
+++ DailyCoding/src/Codility/OddOccurrencesInArray/OddOccurrencesInArray.java
@@ -6,6 +6,8 @@ package codility;
  * 시간복잡도는 N에 공간복잡도는 1에 요구했기 때문에
  * 한 변수 잡아서 Array 훑으라는 말인데
  * 어쨌든 곱셈도 덧셈도 다 해봤는데, 안돼서 비트마스큰가 하다가, exculsive OR가 있었다.
+ * exculsive OR는 짝수는 비크마스크 기준 짝수개는 0으로 수렴하고, 홀수 개 경우에는
+ * 비트가 존재, 남아있는 비트로 수를 판별하는 거라서 홀,짝 판별할때 많이 쓰인다고 합니다.
  *
  */
 public class OddOccurrencesInArray {
