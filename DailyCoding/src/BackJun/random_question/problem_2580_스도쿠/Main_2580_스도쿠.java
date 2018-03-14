c22356d7238523380e433cf91eaa841506023be4
SeogMin <csm0594@gmail.com>
2018-03-15 01:18:00 +0900

    백 트랙킹이라 하지만 재귀문제 아닌가 싶었던 문제.
    1.재귀를 판별하기 위해서도 순서도를 그리든, 절차를 그리자.
    2.성급한 최적화 하지 말기.
    3.git 좋다.

--- DailyCoding/src/BackJun/random_question/problem_2580_스도쿠/Main_2580_스도쿠.java
+++ DailyCoding/src/BackJun/random_question/problem_2580_스도쿠/Main_2580_스도쿠.java
@@ -7,26 +7,85 @@ import java.util.ArrayList;
 
 public class Main_2580_스도쿠 {
 
-    private static boolean[][] possibleYval = new boolean[9][10]; //0은 안쓴다.
-    private static boolean[][] possibleXval = new boolean[9][10]; //0은 안쓴다.
 
+    private static int currentRow;
+    private static int currentCol;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int data[][] = new int[9][9];
+        int[][] data = new int[10][10];
 
-        for(int i=0; i<9; i++){
-            int j = 0;
-            int val = -1;
+        for(int i=1; i<10; i++){
+            int j = 1;
             for(String s:br.readLine().split(" ")){
-                val = Integer.valueOf(s);
-                possibleXval[i][val] = true; //false 애들만 해당 i라인에 가능하다는 말.
-                possibleYval[j][val] = true; //false 것들만 해당 j라인에서 가능하다는 말.
-                data[i][j++] = Integer.valueOf(val);
+                data[i][j++] = Integer.valueOf(s);
             }
         }
+
+        fill(data);
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=1; i<=9; i++){
+            for(int j=1; j<=9; j++){
+                sb.append(data[i][j]+" ");
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb.toString());
     }
 
+    public static boolean isEmpty(int[][] data){
 
+        for(int i=1; i<=9; i++){
+            for(int j=1; j<=9; j++){
+                if(data[i][j]==0){
+                    currentRow = i;
+                    currentCol = j;
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    /***
+     * 백트랙킹이라고 하는데.. 재귀적인 방법을 생각해야 된다.
+     * 재귀를 특히나 어려워 하는건 과정을 종이에 적어보지 않기 때문이다.
+     * 과정을 종이에 적어보면서 반복되는 과정을 재귀에 넣는다라고
+     * 사고 할줄 알아야 하는데.. 참 말이 쉽지.
+     * 이번 달 내 다시 풀어 봐야 된다.
+     * @param data
+     * @return
+     */
+    public static boolean fill(int[][] data) {
+        int r = 0;
+        int c = 0;
+
+        if(isEmpty(data)){
+            return true;
+        }
+
+        r = currentRow;
+        c = currentCol;
+
+        for(int n = 1; n <= 9; n++){
+            if(isRightLineNum(n,r,c,data)){
+                data[r][c] = n;
+                if(fill(data)){
+                    return true;
+                }
+                data[r][c] = 0;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isRightLineNum(int n,int r,int c,int[][] data){
+        if(checkRow(r,n,data) && checkCol(c,n,data) && checkBox(r,c,n,data)){
+            return true;
+        }
+        return false;
+    }
 
     public static boolean checkRow(int r, int n,int[][] data){
         for (int i=1; i<=9; i++) {
