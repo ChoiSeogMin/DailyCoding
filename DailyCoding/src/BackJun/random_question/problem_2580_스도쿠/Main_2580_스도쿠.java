4b33ea75226bbbad8df32ccc23b3bf5d6515f397
SeogMin <csm0594@gmail.com>
2018-03-15 01:23:52 +0900

    순서도 그리거나, 절차 적어 보기.

--- DailyCoding/src/BackJun/random_question/problem_2580_스도쿠/Main_2580_스도쿠.java
+++ DailyCoding/src/BackJun/random_question/problem_2580_스도쿠/Main_2580_스도쿠.java
@@ -14,9 +14,9 @@ public class Main_2580_스도쿠 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int[][] data = new int[10][10];
 
-        for(int i=1; i<10; i++){
+        for (int i = 1; i < 10; i++) {
             int j = 1;
-            for(String s:br.readLine().split(" ")){
+            for (String s:br.readLine().split(" ")) {
                 data[i][j++] = Integer.valueOf(s);
             }
         }
@@ -24,8 +24,8 @@ public class Main_2580_스도쿠 {
         fill(data);
 
         StringBuilder sb = new StringBuilder();
-        for(int i=1; i<=9; i++){
-            for(int j=1; j<=9; j++){
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 9; j++) {
                 sb.append(data[i][j]+" ");
             }
             sb.append("\n");
@@ -36,9 +36,9 @@ public class Main_2580_스도쿠 {
 
     public static boolean isEmpty(int[][] data){
 
-        for(int i=1; i<=9; i++){
-            for(int j=1; j<=9; j++){
-                if(data[i][j]==0){
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 9; j++) {
+                if (data[i][j]==0) {
                     currentRow = i;
                     currentCol = j;
                     return false;
@@ -51,8 +51,9 @@ public class Main_2580_스도쿠 {
     /***
      * 백트랙킹이라고 하는데.. 재귀적인 방법을 생각해야 된다.
      * 재귀를 특히나 어려워 하는건 과정을 종이에 적어보지 않기 때문이다.
+     * 원리를 듣고, 정답소스를 보면 이해가 가지만 그 전에 사고하기는 어렵다.
      * 과정을 종이에 적어보면서 반복되는 과정을 재귀에 넣는다라고
-     * 사고 할줄 알아야 하는데.. 참 말이 쉽지.
+     * 사고 할줄 알아야 하는데..
      * 이번 달 내 다시 풀어 봐야 된다.
      * @param data
      * @return
