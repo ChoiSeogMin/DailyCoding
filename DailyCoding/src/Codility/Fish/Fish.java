876a5b7fd056429560f9e7146cacc49046617a20
SeogMin <csm0594@gmail.com>
2018-03-16 02:17:01 +0900

    Stack을 활용한 Fish잡기

--- DailyCoding/src/Codility/Fish/Fish.java
+++ DailyCoding/src/Codility/Fish/Fish.java
@@ -1,29 +1,31 @@
 package codility.Fish;
 
+import java.util.Stack;
+
 public class Fish {
 
     public static int solution(int[] A,int[] B){
 
-        int rest = A.length;//물고기의 수
+        Stack<Integer> stack = new Stack<>();
+
         int cnt = 0;
 
         for (int i=0; i<A.length; i++) {
-            if (B[i]==0) {
-                continue;
-            } else {//1이면
-                int j = i;
-                while (j+1 < B.length && B[j+1]!= 1) {
-                    if (A[j] > A[j+1]) {
-                        A[j+1] = A[j];
-                        A[j] = -1;
-                        j++;
-                        i = j;
-                    } else { //A[j] < A[j+1]
-                        A[j] = -1;
-                        i = j + 1;
+            if (B[i] == 0) {
+                while(stack.size()!=0){//윗물고기들이 있다면
+                    int upstreamFish = stack.pop();
+                    if(A[i] < upstreamFish){
+                        A[i] = -1; //윗물고기한테 잡아 먹힘
+                        stack.add(upstreamFish);
                         break;
+                    }else{
+                        continue;
                     }
                 }
+            } else { //1
+                stack.add(A[i]);
+                A[i] = -1;
+                //1이면 스택에 담는다.
             }
         }
 
@@ -32,6 +34,7 @@ public class Fish {
                 cnt++;
             }
         }
-        return cnt;
+        //윗물고기 아랫물고기
+        return cnt + stack.size();
     }
 }
