3bcc00e0c3c1a78d02425a08744041acb107057c
SeogMin <csm0594@gmail.com>
2018-03-17 21:35:31 +0900

    Dominator
    시간복잡도 n, 공간복잡도 1인데
    공간복잡도 1은 Codility에서 상수라는 뜻같다.

--- DailyCoding/src/Codility/Dominator/Dominator.java
+++ DailyCoding/src/Codility/Dominator/Dominator.java
@@ -2,10 +2,58 @@ package codility.Dominator;
 
 public class Dominator {
 
-    public static int solution(int[] A){
+    public static int solution(int[] A) {
 
+        int dominator = getDominatorNum(A);
 
+        return isMoreThanHalfSize(dominator, A) ? findFirstDominatorIndex(dominator,A) : -1;
+    }
 
-        return 0;
+    private static int getDominatorNum (int[] A) {
+
+        int size = 0;
+        int dominator = 0;
+
+        for (int value:A) {
+            if (size == 0) {
+                size++;
+                dominator = value;
+            } else {
+                if (value != dominator) {
+                    size--;
+                } else { //value와 dominator가 같다.
+                    size++;
+                }
+            }
+        }
+
+        return dominator;
+    }
+
+    private static boolean isMoreThanHalfSize(int dominator, int[] A) {
+
+        int cnt = 0;
+
+        for (int value:A) {
+            if (value == dominator) {
+               cnt++;
+            }
+        }
+        if (A.length % 2 == 0) {
+            return A.length / 2 < cnt;
+        }
+
+        return (A.length / 2) + 1 <= cnt;
+    }
+
+    private static int findFirstDominatorIndex(int dominator, int[] A) {
+
+        for (int i=0; i<A.length; i++) {
+            if (A[i]==dominator) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 }
--- DailyCoding/src/Codility/Dominator/DominatorTest.java
+++ DailyCoding/src/Codility/Dominator/DominatorTest.java
@@ -8,7 +8,37 @@ class DominatorTest {
 
     @Test
     void solution() {
-        int[] data = {1,2,3,4,1,1,2,2,3,3,4,4,4};
-        assertEquals(3,Dominator.solution(data));
+        int[] data = {1,2,3,4,1,1,2,2,3,3,4,4,4,3,3,3,3,3,3,3,3,3,3,3};
+        assertEquals(2,Dominator.solution(data));
+
+        int[] data2 = {3,4,3,2,3,-1,3,3};
+        assertEquals(0,Dominator.solution(data2));
+
+        int[] data3 = {1,1,2,3,4,5};
+        assertEquals(-1,Dominator.solution(data3));
+
+        int[] data4 = {1,1,1,3,3,3,1};
+        assertEquals(-1,Dominator.solution(data3));
+
+        int[] data5 = {2,2,2,2,2,3};
+        assertEquals( 0,Dominator.solution(data5));
+
+        int[] data6 = {1,2,3,4,5,6,7,2,2,2,2,2,2};
+        assertEquals(1,Dominator.solution(data6));
+
+        int[] data7 = {1,1};
+        assertEquals(0,Dominator.solution(data7));
+
+        int[] data8 = {1};
+        assertEquals(0,Dominator.solution(data8));
+
+        int[] data9 = {2,2,2,1,3,3,1,1,2,2};
+        assertEquals(-1,Dominator.solution(data9));
+
+        int[] data10 = {1,2};
+        assertEquals(-1,Dominator.solution(data10));
+
+        int[] data11 = {1,1,2};
+        assertEquals(0,Dominator.solution(data11));
     }
 }
