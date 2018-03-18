aefc7e5ec2496adb0cdbdec4c1102572c839f939
SeogMin <csm0594@gmail.com>
2018-03-19 00:16:54 +0900

    EquiLeader
    40일 정도 꾸준히하면 몸에 배서, 아주 쉽고 편하게 할 수 있다고 한다.
    정말일까..?

--- DailyCoding/src/Codility/EquiLeader/EquiLeader.java
+++ DailyCoding/src/Codility/EquiLeader/EquiLeader.java
@@ -4,19 +4,24 @@ public class EquiLeader {
 
     public static int solution(int[] A) {
 
+        int dominantNum = getDominatorNum(A);
 
+        if(dominantNum != -1){
+            return isPossibleDominantNum(A,dominantNum) ? getEquipLeaderNum(A,dominantNum,getDominatCount(A,dominantNum)) : 0;
+        }
 
         return 0;
     }
 
-    private static int getDominatorNum(int[] A,int startIndex,int endIndex) {
+    private static int getDominatorNum(int[] A) {
 
         int dominant = 0;
         int size = 0;
 
-        for(int i=startIndex; i<endIndex; i++) {
+        for(int i=0; i<A.length; i++) {
             if(size==0) {
                 dominant = A[i];
+                size++;
             } else {
                 if(dominant == A[i]) {
                   size++;
@@ -29,18 +34,52 @@ public class EquiLeader {
         return size > 0 ? dominant : -1;
     }
 
-}
+    private static boolean isPossibleDominantNum(int[] A,int dominant) {
 
-class Dominator{
+        int cnt = 0;
 
-    public int size;
-    public int dominator;
+        for(int k:A) {
+            if(k == dominant) {
+                cnt++;
+            }
+        }
 
-    public Dominator(int size, int dominator) {
-        this.size = size;
-        this.dominator = dominator;
+        return A.length % 2 == 0 ? cnt > (A.length / 2) : cnt >= (A.length / 2) + 1;
+    }
+
+    private static int getDominatCount(int[] A,int dominant) {
+
+        int cnt = 0;
+
+        for(int k:A) {
+            if(k == dominant) {
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
+
+    /***
+     * 어떻게 이런 생각을 하지? (문제에서 전체 DominantNum 시작해서 Equip을 찾는다는 말인줄은 몰랐다...)
+     * (1, 2~n) (1~2, 3~n) 총합을 totalDomiantCnt에서 판별된 갯수만큼 뺀다는 것 자체가..
+     * 문제를 제대로 이해하고 풀자.
+     */
+    private static int getEquipLeaderNum(int[] A, int dominant, int totalDominantCnt){
+
+        int lcnt = 0; //leader appeard until the index.
+        int ans  = 0; //the number of equi leaders.
+        for (int i = 0; i < A.length; i++) {
+            if(A[i] == dominant) {
+                lcnt++;
+            }
+            //check if the current index is a equi leader
+            int lelems = i + 1;
+            if((lcnt > lelems / 2) && ((totalDominantCnt - lcnt) > (A.length - lelems) / 2)) {
+                ans++;
+            }
+        }
+
+        return ans;
     }
 }
-
-
-
--- DailyCoding/src/Codility/EquiLeader/EquiLeaderTest.java
+++ DailyCoding/src/Codility/EquiLeader/EquiLeaderTest.java
@@ -10,5 +10,17 @@ class EquiLeaderTest {
     void solution() {
         int[] data = {4, 3, 4, 4, 4, 2};
         assertEquals(2,EquiLeader.solution(data));
+
+        int[] data2 = {4,4,2,2,1};
+        assertEquals(0, EquiLeader.solution(data2));
+
+        int[] data3 = {1,1,1,1,2,2,2,3,4,1,1,1};
+        assertEquals(3,EquiLeader.solution(data3));
+
+        int[] data4 = {1,1,1,2,1,1,1,1,2,2,2,2,3,4,1,1,2,3,2,2,2,1,2,2,3,3,4,2,2,2};
+        assertEquals(0,EquiLeader.solution(data4));
+
+        int[] data5 = {1,1,1};
+        assertEquals(2,EquiLeader.solution(data5));
     }
 }
