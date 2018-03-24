2ddae89c2c624e5d2839088556a53db46c8c5dcb
SeogMin <csm0594@gmail.com>
2018-03-25 00:31:28 +0900

    Flags 해결
    testResult안나와서.. 추측하면서 풀었다.
    내일 Refactoring을 해보자.

--- DailyCoding/src/Codility/PrimeAndCompositeNumbers/Flags/Flags.java
+++ DailyCoding/src/Codility/PrimeAndCompositeNumbers/Flags/Flags.java
@@ -2,10 +2,61 @@ package codility.PrimeAndCompositeNumbers.Flags;
 
 public class Flags {
 
-    public static int solution(int[] A){
+    public static int solution(int[] A) {
+        if(A == null) return 0;
+        return getMaxFlagCount(findPeakFlags(A),A);
+    }
 
+    public static int[] findPeakFlags(int[] A) {
 
+        int[] returnArray = new int[A.length];
+        int j = 0;
+        for(int i=1; i<A.length-1; i++) {//j가
+            if(A[i]>A[i-1] && A[i]>A[i+1]) {
+                returnArray[j++] = i;
+            }
+        }
 
-        return 0;
+        return returnArray;
+    }
+
+    public static int getMaxFlagCount(int[] peakIndexArray,int[] A) {
+
+        if(peakIndexArray[0] == 0)
+            return 0;
+
+        int flag = 2;
+        int flagCount = 1;
+        int startFlag = 0;
+        int remoteFlag = 1;
+        int maxResultFlag = 1;
+
+        while(true) {
+
+            if(peakIndexArray[remoteFlag] == 0) {//끝에 도달 했다.
+                if(flagCount != flag) {//만약 flagCount와 falg갯수가 다르면 나간다.
+                    break;
+                }
+            }
+
+            if(peakIndexArray[remoteFlag] - peakIndexArray[startFlag] >= flag) {
+                flagCount++;
+                startFlag = remoteFlag;
+                remoteFlag++;
+
+            } else {
+                remoteFlag++;//다음번으로
+            }
+
+            if(flagCount == flag) {
+                flag++;
+                maxResultFlag = Math.max(maxResultFlag,flagCount);
+                flagCount = 1;
+                startFlag = 0;
+                remoteFlag = 1;
+            }
+        }
+
+        return maxResultFlag;
     }
 }
--- DailyCoding/src/Codility/PrimeAndCompositeNumbers/Flags/FlagsTest.java
+++ DailyCoding/src/Codility/PrimeAndCompositeNumbers/Flags/FlagsTest.java
@@ -6,6 +6,21 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class FlagsTest {
 
+
+    @Test
+    void testFindPeakFlag(){
+
+        int[] testData = {1,5,3,4,3,4,1,2,3,4,6,2};
+        int[] resultData = {1,3,5,10,0,0,0,0,0,0,0,0};
+
+        assertArrayEquals(resultData,Flags.findPeakFlags(testData));
+
+
+        int[] testData2 = {10,3,4,5,11,2,3,4,1,2,12,4,5,6,12,313,23,22};
+        int[] resultData2 = {4,7,10,15,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+        assertArrayEquals(resultData2,Flags.findPeakFlags(testData2));
+    }
+
     @Test
     void solution() {
 
@@ -16,9 +31,12 @@ class FlagsTest {
         assertEquals(3,Flags.solution(data1));
 
         int[] data2 = {10,1,10,1,10,1,10,10,10,10,10,10,10,10,10,10,1,10};
-        assertEquals(1,Flags.solution(data2));
+        assertEquals(2,Flags.solution(data2));
 
         int[] data3 = {1,2,3,4,1,2,3,4,1,2,3,1,2,3,4,1,2,3,1};
         assertEquals(3,Flags.solution(data3));
+
+        int[] data4 = {1,10,1};
+        assertEquals(1,Flags.solution(data4));
     }
 }
