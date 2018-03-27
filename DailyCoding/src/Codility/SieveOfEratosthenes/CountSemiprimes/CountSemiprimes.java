f3f1b16900e12cdaae16aa31b98b596d56352d9f
SeogMin <csm0594@gmail.com>
2018-03-27 23:33:22 +0900

    CountSemiprimes
    우선 커밋

---
+++ DailyCoding/src/Codility/SieveOfEratosthenes/CountSemiprimes/CountSemiprimes.java
@@ -0,0 +1,10 @@
+package codility.SieveOfEratosthenes.CountSemiprimes;
+
+public class CountSemiprimes {
+
+    public static int[] solution(int N, int[] P, int[] Q){
+
+        
+        return null;
+    }
+}
