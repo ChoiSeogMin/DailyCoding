02feccb0f0900aaa09b97edd1ea04ac047310f2c
SeogMin <csm0594@gmail.com>
2018-05-08 16:08:20 +0900

    CountSemiprimes
    correctNess는 맞으나 performance에 문제.

--- DailyCoding/src/Codility/SieveOfEratosthenes/CountSemiprimes/CountSemiprimes.java
+++ DailyCoding/src/Codility/SieveOfEratosthenes/CountSemiprimes/CountSemiprimes.java
@@ -1,10 +1,69 @@
 package codility.SieveOfEratosthenes.CountSemiprimes;
 
+import java.util.*;
+
 public class CountSemiprimes {
 
     public static int[] solution(int N, int[] P, int[] Q){
 
-        
-        return null;
+        return getResultData(getSemiPrimes(N,getPrimes(N)),P,Q);
     }
+
+    public static int[] getResultData(List<Integer> semiprimes, int[] p, int[] q) {
+
+        int[] resultData = new int[p.length];
+        int index = 0;
+
+        for(int i = 0; i < p.length; i++) {
+            int count = 0;
+            for(int j = 0; j < semiprimes.size(); j++) {
+                int data = semiprimes.get(j);
+
+                if (p[i] <= data && data <= q[i]) {
+                    count++;
+                }
+            }
+            resultData[i] = count;
+        }
+
+        return resultData;
+    }
+
+    public static List<Integer> getPrimes(int n) {
+        List<Integer> primes = new ArrayList<>();
+        boolean primeData[] = new boolean[n+1];
+
+        for(int i = 2; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(j==1 && !primeData[i]) {
+                    primes.add(i);
+                    continue;
+                }
+
+                if(i * j > n) {
+                    break;
+                }
+                primeData[i * j] = true;
+            }
+        }
+
+        return primes;
+    }
+
+    public static List<Integer> getSemiPrimes(int n, List<Integer> primes) {
+
+        List<Integer> semiPrimes = new ArrayList<>();
+
+        for(int i = 0; i < primes.size(); i++) {
+            for(int j = i; j < primes.size(); j++) {
+                int semiprimes = primes.get(i) * primes.get(j);
+                if(n < semiprimes)
+                    break;
+                semiPrimes.add(semiprimes);
+            }
+        }
+        Collections.sort(semiPrimes);
+        return semiPrimes;
+    }
+
 }
--- DailyCoding/src/Codility/SieveOfEratosthenes/CountSemiprimes/CountSemiprimesTest.java
+++ DailyCoding/src/Codility/SieveOfEratosthenes/CountSemiprimes/CountSemiprimesTest.java
@@ -1,7 +1,46 @@
 package codility.SieveOfEratosthenes.CountSemiprimes;
 
+
+
+import codility.SieveOfEratosthenes.CountSemiprimes.CountSemiprimes;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CountSemiprimesTest {
 
+
+    @Test
+    public void getPrimeTest() {
+        List<Integer> primes = CountSemiprimes.getPrimes(26);
+        assertThat(primes.size(),is(9));
+    }
+
+    @Test
+    public void getSemiPrimeTest() {
+        List<Integer> primes = CountSemiprimes.getPrimes(26);
+        List<Integer> semiPrimes = CountSemiprimes.getSemiPrimes(26,primes);
+        assertThat(semiPrimes.size(),is(10));
+        System.out.println(semiPrimes);
+    }
+
+    @Test
+    public void getResultData() {
+        List<Integer> primes = CountSemiprimes.getPrimes(26);
+        List<Integer> semiPrimes = CountSemiprimes.getSemiPrimes(26,primes);
+        assertThat(semiPrimes.size(),is(10));
+
+        int[] P = {1,4,16};
+        int[] Q = {26,10,20};
+        int[] resultData = CountSemiprimes.getResultData(semiPrimes,P,Q);
+
+        int[] resultTestData = {10,4,0};
+        assertArrayEquals(resultTestData,resultData);
+        System.out.println(resultData[0]+" "+resultData[1]+" "+resultData[2]);
+
+    }
 }
